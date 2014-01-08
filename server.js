7e5cf45468ee0555f5e13ed4b56e804cb25fddc3
Azkar <sdm350@gmail.com>
2014-01-07 22:22:50 -0800

    Got express off the ground, added dependencies.

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 var express = require("express");
 var app = express();
+
+app.use('/', express.static(__dirname + '/view'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+
+app.listen(3000);
+console.log("listening on port 3000");
